2ec08d1f985cd2eff5add062ca887e519d68cefd
RotKai <89800981+RotKai@users.noreply.github.com>
2022-08-31 11:40:02 -0700

    Update 49. Group Anagrams.cpp

--- string/49. Group Anagrams.cpp
+++ string/49. Group Anagrams.cpp
@@ -7,14 +7,14 @@ public:
     vector<vector<string>> groupAnagrams(vector<string> &words) {
         // write your code here
         unordered_map<string,vector<string>> mp;
-        for (string &word:words){
+        for (const string &word:words){
             string new_word = word;
             sort(new_word.begin(),new_word.end());
             mp[new_word].emplace_back(word);
         }
         vector<vector<string>> ans;
-        for (auto &[v,index]:mp){
-            ans.push_back(index);
+        for (auto &[word,vec]:mp){
+            ans.push_back(vec);
         };
         return ans;
     }
