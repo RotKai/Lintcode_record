adb014bb90c2f29295edf0fff068a8713b8d768c
RotKai <89800981+RotKai@users.noreply.github.com>
2022-09-30 08:12:26 -0700

    Update 588 · Partition Equal Subset Sum.cpp

--- dynamic programming/588 · Partition Equal Subset Sum.cpp
+++ dynamic programming/588 · Partition Equal Subset Sum.cpp
@@ -12,7 +12,7 @@ public:
         }
         if (sum % 2 == 1) return false;
         int mid = sum /= 2;
-        vector<int> dp(mid+1);
+        vector<bool> dp(mid+1);
         dp[0] = 1;
         for (auto num:nums)
             for (int j=mid; j >= num; --j){
